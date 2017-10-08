fcdaca7a869adc1415753de0ad990f678986953e
Pedro Rodrigues <prodrigues1990@gmail.com>
2017-10-08 20:41:53 +0100

    App flow started
    waiting for correct login and assigned flight to display info to pilot

--- Acars/FlightData/FlightDatabase.cs
+++ Acars/FlightData/FlightDatabase.cs
@@ -1,5 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Acars.FlightData
 {
@@ -16,6 +18,63 @@ namespace Acars.FlightData
                     Properties.Settings.Default.Dbpass,
                     Properties.Settings.Default.Database);
             }
+        }
+
+        /// <summary>
+        /// Returns the SHA1 hash string for a given string
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static string StringToSha1Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static bool ValidateLogin(string email, string password)
+        {
+            bool validLogin = false;
+            string sqlStrValidateLogin = "select count(*) from utilizadores where `user_email`=@email and `user_senha`=@password;";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+
+            try
+            {
+                conn.Open();
+
+                MySqlCommand sqlCmd = new MySqlCommand(sqlStrValidateLogin, conn);
+                sqlCmd.Parameters.AddWithValue("@email", email);
+                sqlCmd.Parameters.AddWithValue("@password", StringToSha1Hash(password));
+
+                validLogin = (Convert.ToInt32(sqlCmd.ExecuteScalar()) == 1);
+            }
+            catch (Exception crap)
+            {
+                // log the failure or something, anyway this isn't a good login for sure
+                validLogin = false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return validLogin;
         }
 
         /// <summary>
---
+++ Acars/UI/SettingsFrm.cs
@@ -0,0 +1,41 @@
+﻿using Acars.FlightData;
+using System;
+using System.Windows.Forms;
+
+namespace Acars.UI
+{
+    public partial class SettingsFrm : Form
+    {
+        public SettingsFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SettingsFrm_Load(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reload();
+            txtFlyAtlanticEmail.Text = Properties.Settings.Default.Email;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtFlyAtlanticPassword.Text != "")
+                if (FlightDatabase.ValidateLogin(txtFlyAtlanticEmail.Text, txtFlyAtlanticPassword.Text))
+                {
+                    Properties.Settings.Default.Email = txtFlyAtlanticEmail.Text;
+                    Properties.Settings.Default.Password = txtFlyAtlanticPassword.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Could not validate provided FlyAtlantic credentials.", "Bad login", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+            Properties.Settings.Default.Save();
+        }
+    }
+}
